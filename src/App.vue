aff6eed2de97797fe72cf7db57ea0c995ec35433
zhassbala <besimbekovr@gmail.com>
2022-08-25 12:28:11 +0600

    added save to localstorage & copy to clipboard

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <template>
   <div id="app">
+    <notifications></notifications>
     <section class="refactor">
       <h2>Refactor</h2>
-      <textarea v-model="content" class="box"></textarea>
+      <textarea v-model="content" class="box" @change="handleChange"></textarea>
     </section>
     
     <section class="preview">
       <h2>Preview</h2>
-      <vue-markdown :source="content" class="box"></vue-markdown>
+      <vue-markdown :source="content" class="box" data-markdown-box="1"></vue-markdown>
+      <div class="btn-wrapper">
+        <button @click="copy">Copy HTML to Clipboard</button>
+      </div>
     </section>
   </div>
 </template>
@@ -22,9 +26,22 @@ export default {
   },
   data(){
     return {
-      content: ''
+      content: localStorage.getItem('content') || ''
     }
-  }
+  },
+  methods: {
+    handleChange(){
+      localStorage.setItem('content', this.content)
+    },
+    copy(){
+      const elem = document.querySelector('.box[data-markdown-box="1"]')
+      navigator.clipboard.writeText(elem.innerHTML)
+      this.$notify({
+        type: 'success',
+        text: 'Copied to Clipboard!'
+      })
+    }
+  },
 }
 </script>
 
@@ -41,6 +58,7 @@ export default {
   flex: 1 1 0;
   display: flex;
   flex-direction: column;
+  grid-gap: 0.5rem;
   h2{
     height: 50px;
     flex-basis: 50px;
@@ -54,6 +72,15 @@ export default {
     border-radius: .5rem;
     resize: vertical;
     padding: .5rem;
+
+  }
+  textarea.box{
+    margin-bottom: 2rem;
+  }
+  .btn-wrapper {
+    display: flex;
+    align-items: center;
+    justify-content: center;
   }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
+import Notifications from 'vue-notification'
 
 Vue.config.productionTip = false
+Vue.use(Notifications)
 
 new Vue({
   store,
